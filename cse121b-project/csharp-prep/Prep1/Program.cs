3b188c4b80b325bd69d8f93df513a7b5ba626a8c
JPgitidaho <per22030@byui.edu>
2023-09-20 00:29:40 -0300

    edit

--- cse121b-project/csharp-prep/Prep1/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
-}
--- cse121b-project/csharp-prep/Prep2/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
-}
