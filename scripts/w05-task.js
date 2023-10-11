c02fbaffc1702efcc7a4bf6db2e95396ec656851
JPgitidaho <122384707+JPgitidaho@users.noreply.github.com>
2023-10-11 19:45:15 -0300

    edit code

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -3,59 +3,63 @@ const templesElement = document.querySelector("#temples");
 let templeList = [];
 
 /* async displayTemples Function */
-const displayTemples = (temples) => {
-  // Clear existing output
-  templesElement.innerHTML = '';
+function displayTemples(templeList) {
+  const templeDiv = document.querySelector("#temples");
+  templeDiv.innerHTML = ''; // Clear the temple container
 
-  // Create a wrapper for the temple cards to control layout
-  const templeWrapper = document.createElement('div');
-  templeWrapper.classList.add('temple-wrapper');
+  templeList.forEach((temple) => {
+    const templeWrapper = document.createElement('div');
+    templeWrapper.classList.add('temple-wrapper');
 
-  // Process each temple and create HTML elements
-  temples.forEach((temple) => {
-    const article = document.createElement('article');
-    
-    const h3 = document.createElement('h3');
-    h3.textContent = temple.templeName;
-    
+    const templeNameH3 = document.createElement("h3");
+    templeNameH3.innerText = temple.templeName;
+    templeWrapper.appendChild(templeNameH3);
+
+    const locationP = document.createElement("p");
+    locationP.innerText = "Location: " + temple.location;
+    templeWrapper.appendChild(locationP);
+
+    const dedicatedP = document.createElement("p");
+    dedicatedP.innerText = "Dedicated: " + temple.dedicated;
+    templeWrapper.appendChild(dedicatedP);
+
+    const areaP = document.createElement("p");
+    areaP.innerText = "Area: " + temple.area + " square feet";
+    templeWrapper.appendChild(areaP);
+
     const img = document.createElement('img');
     img.src = temple.imageUrl;
     img.alt = temple.templeName;
-    img.setAttribute('location', temple.location);
 
-    // Append elements to the article
-    article.appendChild(h3);
-    article.appendChild(img);
+    // Add data attributes for future reference
+    img.setAttribute('data-location', temple.location);
+    img.setAttribute('data-dedicated', temple.dedicated);
+    img.setAttribute('data-area', temple.area);
 
-    // Append the article to the temple wrapper
-    templeWrapper.appendChild(article);
+    // Append elements to the temple container
+    templeWrapper.appendChild(img);
+    templeDiv.appendChild(templeWrapper);
   });
-
-  // Append the temple wrapper to the global container
-  templesElement.appendChild(templeWrapper);
-};
-
-/* async getTemples Function using fetch() */
-const getTemples = async () => {
+}
+/* async getTempleData Function using fetch() */
+async function getTempleData() {
   try {
     const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
+    templeList = await response.json();
+    if (response.ok) {
+      displayTemples(templeList); // Mostrar los templos
+    } else {
+      console.log("DATA COULD NOT BE FETCHED");
     }
-
-    const data = await response.json();
-
-    templeList = data;
-    displayTemples(templeList);
+    console.log("TEMPLE DATA:", templeList);
   } catch (error) {
     console.error("Error fetching temple data:", error);
   }
-};
+}
 
 /* reset Function */
 const reset = () => {
-  templesElement.innerHTML = '';
+  templesElement.innerHTML = ''; // Clear the temple container
 };
 
 /* Function to sort temples */
@@ -78,7 +82,7 @@ const sortBy = (temples) => {
       displayTemples(temples);
       break;
   }
-};
+}
 
 /* Event Listener for the Sort By dropdown */
 document.getElementById("sortBy").addEventListener("change", () => {
@@ -86,4 +90,4 @@ document.getElementById("sortBy").addEventListener("change", () => {
 });
 
 /* Initialize by fetching temple data */
-getTemples();
+getTempleData();
